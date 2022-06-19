4319c4a4179328da7f790f05c216428f60191be6
YuraKProgrammer <yura.kalantyr@mail.ru>
2022-06-19 19:00:47 +0300

    Связь между CalendarService и AuthService

--- Calendar.Tests/EventServiceTest.cs
+++ Calendar.Tests/EventServiceTest.cs
@@ -1,20 +1,46 @@
 ﻿using Calendar.Models;
 using Calendar.WebService.Services;
+using Kalantyr.Auth.Client;
+using Moq;
 using NUnit.Framework;
 
 namespace Calendar.Tests
 {
     public class EventServiceTest
     {
+        private readonly Mock<IAppAuthClient> _appAuthClient = new Mock<IAppAuthClient>();
+
         [TestCase(null)]
         [TestCase("")]
         [TestCase(" ")]
         [TestCase("   ")]
         public async Task GetCount_Token_Test(string token)
         {
-            var service = new EventService();
+            var service = new EventService(_appAuthClient.Object);
             var result = await service.GetCountAsync(new DateTime(), new DateTime(), token, CancellationToken.None);
             Assert.AreEqual(Errors.TokenNotFound.Code,result.Error.Code);
+        }
+
+        [TestCase("12345",true)]
+        [TestCase("54321",false)]
+        public async Task GetCount_UserId_Test(string token, bool success)
+        {
+            _appAuthClient
+                .Setup(ac => ac.GetUserIdAsync("12345", It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new Kalantyr.Web.ResultDto<uint> {Result=123});
+            _appAuthClient
+                .Setup(ac => ac.GetUserIdAsync("54321", It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new Kalantyr.Web.ResultDto<uint> { Error=new Kalantyr.Web.Error {Code="E"}});
+            var service = new EventService(_appAuthClient.Object);
+            var result = await service.GetCountAsync(new DateTime(), new DateTime(), token, CancellationToken.None);
+            if (success == true)
+            {
+                Assert.IsNull(result.Error);
+            }
+            else
+            {
+                Assert.AreEqual("E", result.Error.Code);
+            }
         }
     }
 }
--- Calendar.WebService/Services/EventService.cs
+++ Calendar.WebService/Services/EventService.cs
@@ -1,15 +1,33 @@
 ﻿using Calendar.Models;
+using Kalantyr.Auth.Client;
 using Kalantyr.Web;
 
 namespace Calendar.WebService.Services
 {
     public class EventService
     {
+        private readonly IAppAuthClient _appAuthClient;
         public async Task<ResultDto<int>> GetCountAsync(DateTime fromDate, DateTime toDate, string userToken, CancellationToken cancellationToken)
         {
             if (string.IsNullOrWhiteSpace(userToken))
                 return new ResultDto<int> { Error = Errors.TokenNotFound };
-            return new ResultDto<int> { Result = 5 };
+
+            var getUserIdResult = await _appAuthClient.GetUserIdAsync(userToken, cancellationToken);
+            if (getUserIdResult.Error!=null)
+                return new ResultDto<int> { Error = getUserIdResult.Error };
+
+            var count = 0;
+            if (getUserIdResult.Result == 3)
+                count = 30;
+            if (getUserIdResult.Result == 2)
+                count = 20;
+
+            return new ResultDto<int> { Result = count };
+        }
+
+        public EventService(IAppAuthClient appAuthClient)
+        {
+            _appAuthClient = appAuthClient;
         }
     }
 }
