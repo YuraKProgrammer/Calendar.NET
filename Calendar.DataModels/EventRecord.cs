2479b643428bffb0f0007d83c37eb51b6f57f506
S.Kalantyr <kalantyr@yandex.ru>
2022-06-30 15:38:36 +0300

    Начало проекта для работы с базой данных MS-SQL

--- Calendar.DataModels/EventRecord.cs
+++ Calendar.DataModels/EventRecord.cs
@@ -1,9 +1,14 @@
-﻿namespace Calendar.DataModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calendar.DataModels
 {
     public class EventRecord
     {
         public uint Id { get; set; }
+
+        [MaxLength(255)]
         public string Name { get; set; }
+
         /// <summary>
         /// Дата начала события
         /// </summary>
